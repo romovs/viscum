romovs/viscum

commit 709afded050996d09fc27d0b05616f497c696271

fixed windows not being rendered untill the mouse moved.

compositor/compositor.go
@@ -8,6 +8,7 @@ import (
 	"container/list"
 	"toolkit/base"
 	"gfx"
+	"time"
 )
 
 type Compositor struct {
@@ -30,8 +31,6 @@ func CreateCompositor(fb *fbdev.Framebuffer) (*Compositor) {
 		CompositorRelease:	make(chan bool, 1),
 	} 
 
-	c.CompositorRelease<- false
-	
 	return c
 }
 
@@ -40,7 +39,7 @@ func (c *Compositor) Compose() {
 
     for {
         <-c.InvMsgPipe
-				
+
 		<-c.MouseWait
 
 		// render desktop, windows, and child elements
@@ -69,6 +68,10 @@ func flush(c *Compositor) {
 func (c *Compositor) RegisterElement(e *base.Element) {
 	e.CompRemoveHdnr = c.RemoveElement
 	c.WindowList.PushFront(e)
+	
+	// do not wait for mouse when new element is registered
+	c.InvMsgPipe <- time.Now().UnixNano()
+	c.MouseWait<- false
 }
 
 func (c *Compositor) RemoveElement(id uint64) {

mouse/_defines.go
@@ -1,0 +1,45 @@
+package mouse
+
+/*
+#include <linux/input.h>
+*/
+import "C"
+
+/*
+ * linux/input.h
+ */
+
+const (
+	EV_SYN uint16		= C.EV_SYN 
+	EV_KEY uint16		= C.EV_KEY 
+	EV_REL uint16		= C.EV_REL
+	EV_ABS uint16		= C.EV_ABS
+	EV_MSC uint16		= C.EV_MSC
+	EV_SW uint16		= C.EV_SW
+	EV_LED uint16		= C.EV_LED
+	EV_SND uint16		= C.EV_SND
+	EV_REP uint16		= C.EV_REP
+	EV_FF uint16		= C.EV_FF
+	EV_PWR uint16		= C.EV_PWR
+	EV_FF_STATUS uint16	= C.EV_FF_STATUS
+	EV_MAX uint16		= C.EV_MAX
+	EV_CNT uint16		= C.EV_CNT
+)
+
+const(
+	REL_X uint16		= C.REL_X
+	REL_Y uint16		= C.REL_Y
+	REL_Z uint16		= C.REL_Z
+	REL_RX uint16		= C.REL_RX
+	REL_RY uint16		= C.REL_RY
+	REL_RZ uint16		= C.REL_RZ
+	REL_HWHEEL uint16	= C.REL_HWHEEL
+	REL_DIAL uint16		= C.REL_DIAL
+	REL_WHEEL uint16	= C.REL_WHEEL
+	REL_MISC uint16		= C.REL_MISC
+	REL_MAX uint16		= C.REL_MAX
+	REL_CNT uint16		= C.REL_CNT
+)
+
+type inputEvent C.struct_input_event
+type timeVal C.struct_timeval

mouse/defines.go
@@ -1,0 +1,47 @@
+// Created by cgo -godefs - DO NOT EDIT
+// cgo -godefs _defines.go
+
+package mouse
+
+const (
+	EV_SYN		uint16	= 0x0
+	EV_KEY		uint16	= 0x1
+	EV_REL		uint16	= 0x2
+	EV_ABS		uint16	= 0x3
+	EV_MSC		uint16	= 0x4
+	EV_SW		uint16	= 0x5
+	EV_LED		uint16	= 0x11
+	EV_SND		uint16	= 0x12
+	EV_REP		uint16	= 0x14
+	EV_FF		uint16	= 0x15
+	EV_PWR		uint16	= 0x16
+	EV_FF_STATUS	uint16	= 0x17
+	EV_MAX		uint16	= 0x1f
+	EV_CNT		uint16	= 0x20
+)
+
+const (
+	REL_X		uint16	= 0x0
+	REL_Y		uint16	= 0x1
+	REL_Z		uint16	= 0x2
+	REL_RX		uint16	= 0x3
+	REL_RY		uint16	= 0x4
+	REL_RZ		uint16	= 0x5
+	REL_HWHEEL	uint16	= 0x6
+	REL_DIAL	uint16	= 0x7
+	REL_WHEEL	uint16	= 0x8
+	REL_MISC	uint16	= 0x9
+	REL_MAX		uint16	= 0xf
+	REL_CNT		uint16	= 0x10
+)
+
+type inputEvent struct {
+	Time	timeVal
+	Type	uint16
+	Code	uint16
+	Value	int32
+}
+type timeVal struct {
+	Sec	int64
+	Usec	int64
+}

viscum.go
@@ -31,7 +31,7 @@ func main() {
 
 	// initialize compositor
 	cmp := compositor.CreateCompositor(fb)
-	go cmp.Compose()
+
 	
 	// TODO: initialize touchscreen handler
 
@@ -46,6 +46,8 @@ func main() {
 	
 	mp := ms.CreatePointer(fb, cmp.InvMsgPipe)
 	cmp.RegisterMousePointer(&mp.Element)
+
+	go cmp.Compose()
 
 	// create desktop
 	desk, err := toolkit.CreateDesktop(fb, ms, cmp.InvMsgPipe, 50, 107, 89, gfx.A_OPAQUE)

commit 46c872a243102eb1c37f4d1d154eb7327834a7c2

fixed regression introduced in 709afded.

compositor/compositor.go
@@ -8,7 +8,6 @@ import (
 	"container/list"
 	"toolkit/base"
 	"gfx"
-	"time"
 )
 
 type Compositor struct {
@@ -30,7 +29,9 @@ func CreateCompositor(fb *fbdev.Framebuffer) (*Compositor) {
 		MouseWait: 			make(chan bool, 1),
 		CompositorRelease:	make(chan bool, 1),
 	} 
-
+	
+	c.CompositorRelease<- false
+	
 	return c
 }
 
@@ -68,10 +69,6 @@ func flush(c *Compositor) {
 func (c *Compositor) RegisterElement(e *base.Element) {
 	e.CompRemoveHdnr = c.RemoveElement
 	c.WindowList.PushFront(e)
-	
-	// do not wait for mouse when new element is registered
-	c.InvMsgPipe <- time.Now().UnixNano()
-	c.MouseWait<- false
 }
 
 func (c *Compositor) RemoveElement(id uint64) {